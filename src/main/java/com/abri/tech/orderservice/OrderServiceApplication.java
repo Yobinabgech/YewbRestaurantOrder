402cc1172dd763014c89359b34644be9965cec0d
Yewbedar Gizaw <92902098+Yobinabgech@users.noreply.github.com>
2021-12-14 15:05:09 -0500

    My second commit

--- src/main/java/com/abri/tech/orderservice/OrderServiceApplication.java
+++ src/main/java/com/abri/tech/orderservice/OrderServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderServiceApplication {
+public class OrderServiceApplication{
 
 	public static void main(String[] args) {
+
+	System.out.println("Hello Yewb");
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
